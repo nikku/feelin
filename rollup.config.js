d3e0232a1930edf5545808ada44bfd3e47eabfda
Nico Rehwaldt <git_nikku@nixis.de>
2019-12-27 21:36:01 +0100

    chore(project): publish UMD, CJS and module builds

--- rollup.config.js
+++ rollup.config.js
@@ -4,16 +4,55 @@ import commonjs from '@rollup/plugin-commonjs';
 
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: './src/index.js',
-  output: {
-    format: 'cjs',
-    file: './dist/index.js'
-  },
-  external(id) { return !/^[\.\/]/.test(id) },
-  plugins: [
-    process.env.NODE_ENV === 'production' && terser(),
-    nodeResolve(),
-    commonjs()
-  ]
+
+import pkg from './package.json';
+
+function pgl(plugins=[]) {
+  return plugins;
 }
+
+const srcEntry = pkg.source;
+
+const umdDist = pkg['umd:main'];
+
+const umdName = 'ModdleXML';
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: srcEntry,
+    output: {
+      file: umdDist.replace(/\.js$/, '.prod.js'),
+      format: 'umd',
+      name: umdName
+    },
+    plugins: pgl([
+      nodeResolve(),
+      commonjs(),
+      terser()
+    ])
+  },
+  {
+    input: srcEntry,
+    output: {
+      file: umdDist,
+      format: 'umd',
+      name: umdName
+    },
+    plugins: pgl([
+      nodeResolve(),
+      commonjs()
+    ])
+  },
+  {
+    input: srcEntry,
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    external: [
+      'lezer'
+    ],
+    plugins: pgl()
+  }
+];

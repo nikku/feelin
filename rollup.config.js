9b15f724aa49fcba45cdbcfe6193d275743c21c7
Nico Rehwaldt <git_nikku@nixis.de>
2022-08-06 17:59:31 +0200

    chore: remove UMD build

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,11 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
-
 
 import pkg from './package.json';
 
+
 const srcEntry = pkg.source;
 
-const umdDist = pkg['umd:main'];
-
-const umdName = 'Feelin';
-
 export default [
-
-  // browser-friendly UMD build
-  {
-    input: srcEntry,
-    output: {
-      file: umdDist.replace(/\.js$/, '.prod.js'),
-      format: 'umd',
-      name: umdName
-    },
-    plugins: [
-      nodeResolve(),
-      commonjs(),
-      typescript(),
-      terser()
-    ]
-  },
-  {
-    input: srcEntry,
-    output: {
-      file: umdDist,
-      format: 'umd',
-      name: umdName
-    },
-    plugins: [
-      nodeResolve(),
-      commonjs(),
-      typescript()
-    ]
-  },
   {
     input: srcEntry,
     output: [

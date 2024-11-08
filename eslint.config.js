90f33e6ec00a957fc2696f7c3e8639cf65b5cb58
Nico Rehwaldt <git_nikku@nixis.de>
2024-11-08 21:14:08 +0100

    chore: update linter config to eslint@9

---
+++ eslint.config.js
@@ -0,0 +1,87 @@
+import bpmnIoPlugin from 'eslint-plugin-bpmn-io';
+
+import typescriptPlugin from 'typescript-eslint';
+
+const files = {
+  build: [
+    '*.js',
+    'tasks/*.js'
+  ],
+  test: [
+    'test/**/*.js',
+    'test/**/*.ts',
+    'test/**/*.cjs'
+  ],
+  ignored: [
+    'dist',
+    'tmp'
+  ]
+};
+
+export default [
+  {
+    'ignores': files.ignored
+  },
+
+  // lib
+  ...bpmnIoPlugin.configs.recommended.map(config => {
+
+    return {
+      ...config,
+      ignores: [
+        ...files.build,
+        ...files.test
+      ]
+    };
+  }),
+
+  // build + test
+  ...bpmnIoPlugin.configs.node.map(config => {
+
+    return {
+      ...config,
+      files: [
+        ...files.build,
+        ...files.test
+      ]
+    };
+  }),
+
+  // test
+  ...bpmnIoPlugin.configs.mocha.map(config => {
+
+    return {
+      ...config,
+      files: files.test
+    };
+  }),
+
+  ...typescriptPlugin.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error', {
+          'varsIgnorePattern': '^_',
+          'argsIgnorePattern': '^_',
+          'caughtErrorsIgnorePattern': '^_'
+        }
+      ],
+      '@typescript-eslint/no-explicit-any': [
+        'error', {
+          'ignoreRestArgs': true
+        }
+      ]
+    },
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname
+      }
+    }
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-expressions': 'off'
+    },
+    files: files.test
+  }
+];
